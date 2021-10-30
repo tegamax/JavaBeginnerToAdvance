519eab99eb98c122e12fafc44305dbb2cc0ff949
Tega Tega <tegaslink@gmail.com>
2021-10-30 14:08:50 -0700

    hacktoberfest-accepted

---
+++ IsPrime.java
@@ -0,0 +1,26 @@
+public class isPrime {
+	public static double absoluteValue(double valu) {
+		//double valu;
+		if (valu <  0 ) {
+			return valu;
+		}else {
+			return valu;
+		}
+	}
+	public static void main(String[] args) {
+		//absoluteValue(5.0);
+		System.out.println(absoluteValue(-7.1));
+	}
+}
+
+/*	public static boolean main(String[] args) {
+public boolean isPrime(int n) {
+boolean isPrime;
+int testNum = 5;
+if (testNum/testNum == 0) {
+	return isPrime;
+}
+}
+
+}
+*/
